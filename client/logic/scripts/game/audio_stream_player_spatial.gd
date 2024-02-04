f044a7a7e95eb253fe2002559d92aaa2f7012c96
HunterNN <stephan.kessler@outlook.com>
2024-02-04 14:45:56 +0100

    fix(client): some minor fixes (unused files, wrong type, added python cache to gitignore) (#35)

--- client/logic/scripts/game/audio_stream_player_spatial.gd
+++ client/logic/scripts/game/audio_stream_player_spatial.gd
@@ -2,9 +2,10 @@ extends AudioStreamPlayer2D
 
 var base_volume: float = volume_db
 
+var _sound: GlobalSound = Sound
 
 func _ready() -> void:
-	Sound.settings_changed.connect(_on_settings_changed)
+	_sound.settings_changed.connect(_on_settings_changed)
 	_on_settings_changed(ProjectSettings.get_setting("game/sound_volume") as float)
 
 
