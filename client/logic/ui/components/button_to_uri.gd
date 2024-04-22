b35ecd8858e5de99462b9f7aadaf39a02f2816b3
Jaume <jaume.singlavalls@gmail.com>
2024-04-22 19:05:28 +0200

    feat(client): small composition of small code and some improvements (#88)
    * feat(client): small composition of small code and some improvements
    
    * feat(client): updated variable name
    
    * feat(client): updated variable name and fixing the scene

---
+++ client/logic/ui/components/button_to_uri.gd
@@ -0,0 +1,9 @@
+extends GameButton
+
+@export var uri_to_go: String
+
+func _pressed() -> void:
+	super._pressed()
+	var error : Error = OS.shell_open(uri_to_go)
+	if error:
+		printerr("Error(%d) opening the link '%s' " % [error, uri_to_go])
