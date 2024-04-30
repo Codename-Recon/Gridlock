0118b8f5420e8081792813395ec343a5ba1e8e9a
HunterNN <stephan.kessler@outlook.com>
2024-04-30 07:05:44 +0200

    feat(client): changed sound settings to use bus system and removed unused code (#95)

--- client/logic/globals/music.gd
+++ client/logic/globals/music.gd
@@ -4,6 +4,7 @@ signal settings_changed
 signal music_finished
 
 const LOWEST_VOLUME: float = -50.0
+const NORMAL_VOLUME: float = 0.0
 
 var player: Array[AudioStreamPlayer]
 
@@ -13,9 +14,11 @@ var _tween2: Tween
 
 func _ready() -> void:
 	player = [$Player1, $Player2]
-	var volume: float = ProjectSettings.get_setting("game/music_volume")
-	player[0].volume_db = volume
-	player[1].volume_db = volume
+	var volume: float = 0
+	# Set music volume with settings value
+	var value: float = ProjectSettings.get_setting("game/music_volume")
+	var idx: int = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(idx, value)
 
 
 func change_music(music: AudioStream) -> void:
@@ -27,21 +30,14 @@ func change_music(music: AudioStream) -> void:
 	var front: AudioStreamPlayer = player.pop_front()
 	player.append(front)
 	player[0].stream = music
-	var volume: float = ProjectSettings.get_setting("game/music_volume")
 	var time: float = ProjectSettings.get_setting("global/music_tween_time")
 	player[0].volume_db = LOWEST_VOLUME
 	player[0].play()
 	_tween1 = create_tween()
-	_tween1.tween_property(player[0], "volume_db", volume, time)
+	_tween1.tween_property(player[0], "volume_db", NORMAL_VOLUME, time)
 	_tween2 = create_tween()
 	_tween2.tween_property(player[1], "volume_db", LOWEST_VOLUME, time)
 	_tween2.tween_callback(player[1].stop)
-
-
-func _on_music_settings_changed() -> void:
-	var volume: float = ProjectSettings.get_setting("game/music_volume")
-	player[0].volume_db = volume
-	player[1].volume_db = volume
 
 
 func _on_player_1_finished() -> void:
--- client/logic/globals/sound.gd
+++ client/logic/globals/sound.gd
@@ -1,8 +1,12 @@
 class_name GlobalSound
 extends Node
 
-signal settings_changed
-
 
 func play(sound: String) -> void:
 	(get_node(sound) as AudioStreamPlayer).play()
+
+func _ready() -> void:
+	# Set Sound volume with settings value
+	var value: float = ProjectSettings.get_setting("game/sound_volume")
+	var idx: int = AudioServer.get_bus_index("Sound")
+	AudioServer.set_bus_volume_db(idx, value)
