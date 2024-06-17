82a50cac18608bd546fa63526af132ef199b1f39
Stephan Kessler <stephan.kessler@outlook.com>
2024-06-17 19:45:39 +0200

    feat(git): added start point and offset to move arrow

--- client/logic/ui/move_arrow.gd
+++ client/logic/ui/move_arrow.gd
@@ -6,6 +6,8 @@ extends Path2D
 @export var up_arrow: Texture
 @export var vertical_line: Texture
 @export var up_corner: Texture
+@export var up_start: Texture
+@export var offset: Vector2
 
 
 func _ready() -> void:
@@ -25,18 +27,23 @@ func create_arrow() -> void:
 		direction = curve.get_point_position(i + 1) - curve.get_point_position(i)
 		var sprite: Sprite2D = Sprite2D.new()
 		if old_direction == Vector2.ZERO or abs(old_direction.angle_to(direction)) < 0.1:
-			sprite.texture = vertical_line
+			if i == 0:
+				# Sets the start point, which should be behind the unit
+				sprite.texture = up_start
+				sprite.z_index = -1
+			else:
+				sprite.texture = vertical_line
 		else:
 			sprite.texture = up_corner
 			if old_direction.angle_to(direction) < 0:
 				sprite.scale.x *= -1
 		sprite.rotation = direction.angle() + PI / 2
-		sprite.global_position = curve.get_point_position(i)
+		sprite.global_position = curve.get_point_position(i) + offset
 		add_child(sprite)
 	# add arrow head
 	if curve.point_count > 1:
 		var sprite: Sprite2D = Sprite2D.new()
 		sprite.texture = up_arrow
 		sprite.rotation = direction.angle() + PI / 2
-		sprite.global_position = curve.get_point_position(curve.point_count - 1)
+		sprite.global_position = curve.get_point_position(curve.point_count - 1) + offset
 		add_child(sprite)
