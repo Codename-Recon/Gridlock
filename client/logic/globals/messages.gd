2f212eefc0c0b5e746b1e1c44e4e4e9dda091ec5
jaume <jaume.singlavalls@gmail.com>
2024-06-24 20:27:15 +0200

    modifying the messages
    # Conflicts:
    #	client/levels/menu.tscn

--- client/logic/globals/messages.gd
+++ client/logic/globals/messages.gd
@@ -6,6 +6,9 @@ signal action_done
 var _reload_to_menu: bool = false
 
 @onready var accept_dialog: AcceptDialog = $AcceptDialog
+@onready var notification: Control = $Notification
+@onready var notifications_list: VBoxContainer = $NotificationsList
+
 
 
 func spawn(title: String, text: String, reload_to_menu: bool = false) -> void:
@@ -19,6 +22,14 @@ func spawn(title: String, text: String, reload_to_menu: bool = false) -> void:
 		get_viewport().get_visible_rect().size.y / 2.0 - accept_dialog.size.x / 2.0
 	)
 	_reload_to_menu = reload_to_menu
+	
+func spawn_window(title: String, content: Node, buttons: Array[Button]) -> void:
+	pass
+	
+func spawn_notification(title: String, buttons: Array[Button], timeout: int = 0) -> void:
+	var notice: Control = notification.duplicate()
+	notice.show()
+	notifications_list.add_child(notice)
 
 
 func _on_accept_dialog_confirmed() -> void:
