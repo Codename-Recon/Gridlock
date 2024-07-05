54be40bfff50f096a4a2b0b7c7c7d81657e70a19
Jaume <singlavalls@media-saturn.com>
2024-07-05 17:51:53 +0200

    feat(client): Updated translations

--- client/logic/ui/new_version_checker.gd
+++ client/logic/ui/new_version_checker.gd
@@ -12,9 +12,9 @@ class Version:
 	var minor: int
 	var patch: int
 	var valid: bool = false
-	
+
 	static var VERSION_REGEX : RegEx = RegEx.create_from_string("v(?<major>[0-9]+)\\.(?<minor>[0-9]+)\\.(?<patch>[0-9]+)")
-	
+
 	func _init(data: String)->void:
 		var found: RegExMatch = VERSION_REGEX.search(data)
 		if found:
@@ -34,7 +34,7 @@ class Version:
 			else:
 				return;
 			self.valid = true
-				
+
 	func compare(other: Version)->int:
 		if major > other.major:
 			return -1
@@ -50,7 +50,7 @@ class Version:
 			return 1
 		else:
 			return 0
-			
+
 	func _to_string() -> String:
 		return "v%d.%d.%d" % [major, minor, patch]
 
@@ -64,10 +64,10 @@ func _filter_release(release: Dictionary)->bool:
 	var tag_name: String = release.get('tag_name', '')
 	var draft: bool = release.get('draft', false)
 	var prerelease: bool = release.get('prerelease', false)
-	
+
 	var found : RegExMatch = Version.VERSION_REGEX.search(tag_name)
 	return found != null && not draft && not prerelease
-	
+
 
 func _on_request_completed(result:int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	var release_list : Array = JSON.parse_string(body.get_string_from_utf8())
@@ -77,32 +77,27 @@ func _on_request_completed(result:int, response_code: int, headers: PackedString
 		compare_with_current(last_release, current_version_label.text)
 
 func load_release_data(release_data: Dictionary, version: Version) -> void:
-	var notification_title : String = tr("Version {version} available!", "Notification title for a new version").format({version=release_data.get("tag_name")})
+	var notification_title : String = tr("VERSION_NOTIFICATION_TITLE", "Notification title for a new version").format({version=release_data.get("tag_name")})
 	var open: ButtonToUri = ButtonToUri.new()
-	open.text = tr("Open", "Open notification in the browser")
-	
+	open.text = tr("VERSION_NOTIFICATION_OPEN", "Open release in the browser, button in notification")
+
 	var details: Button = Button.new()
-	details.text = tr("More details", "Open details of the notification")
-	
+	details.text = tr("VERSION_NOTIFICATION_MODE_DETAILS", "Open details of the notification")
+
 	var notification : NotificationPanel = messages.spawn_notification(notification_title, [open, details], 0)
-	
+
 	open.uri_to_go = release_data.get("html_url")
-	
+
 	# Not sure about if we should or not close the notification
 	open.pressed.connect(func () -> void: notification.close())
 	details.pressed.connect(func ()->void: notification.close(); open_version_summary(release_data))
-	
-	#new_version.show()
-	# print(release_data)
-	#version_label.text = release_data.get("tag_name")
-	#body_text.text = release_data.get("body", "No description in the last version")
-	#markdown_text.markdown_text = release_data.get("body", "This release doesn't contain any description")
-	#open_release.uri_to_go = release_data.get("html_url")
+
+
 func open_version_summary(release_data: Dictionary) -> void :
 	var title : String =  release_data.get("tag_name")
 	var markdown_content : String = release_data.get("body", "No description in the last version")
 	var button : ButtonToUri = ButtonToUri.new()
-	button.text = tr("Open release", "Open release in the browser")
+	button.text = tr("VERSION_WINDOW_OPEN", "Open release in the browser")
 	button.uri_to_go = release_data.get("html_url")
 	messages.spawn_markdown(title, markdown_content, button)
 
