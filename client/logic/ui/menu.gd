2db185a97275b68fb0bffe5b67cc853dc98f73a4
Jaume <jaume.singlavalls@gmail.com>
2024-07-12 15:41:32 +0200

    feat(client): disabled translations for labels and texts that doesn't have translations (#129)
    * feat(client): disabled translations for labels and texts that doesn't have translations
    
    * feat(client): Big improvement using an ignored gdscript
    
    * feat(client): added game_logic to autogeneration base.pot
    
    * feat(client): missing texts from menus
    
    * chore(client): enhanced text
    
    ---------
    
    Co-authored-by: Jaume <singlavalls@media-saturn.com>
    Co-authored-by: HunterNN <stephan.kessler@outlook.com>

--- client/logic/ui/menu.gd
+++ client/logic/ui/menu.gd
@@ -2,6 +2,8 @@
 class_name Menu
 extends Control
 
+# @see https://github.com/godotengine/godot-proposals/issues/10139 for a proposal of how to improve
+# this text translation, as it can be a problem when changing the language inside the game
 @export var title: String = "Menu":
 	set(value):
 		($Label as Label).text = tr(value)
