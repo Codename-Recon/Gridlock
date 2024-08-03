65b94c4b6ed75d2fd5ca2a743b23be51106e4c47
Jaume <singlavalls@media-saturn.com>
2024-08-03 15:10:25 +0200

    chore(client): Improving the feature with P/R suggestions

--- client/logic/ui/round_label.gd
+++ client/logic/ui/round_label.gd
@@ -4,7 +4,8 @@ extends Label
 @export var player_name: String:
 	set(value):
 		player_name = value
-		text = "%s %s %s" % [tr("PLAYER"), str(value), tr("TURN")]
+		# text = "%s %s %s" % [tr("PLAYER"), str(value), tr("TURN")]
+		text = tr("PLAYER_TURN", "Round announcement, showing which player's turn it is.").format({player_name= value})
 
 
 func _ready() -> void:
