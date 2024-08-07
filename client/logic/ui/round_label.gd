7be6fe267c088cc8896846e20d9f95bac80fccf4
Jaume <jaume.singlavalls@gmail.com>
2024-08-07 22:13:33 +0200

    chore(client): finishing translations cleanup (#136)
    * chore(client): finishing translations cleanup
    
    * chore(client): Improving the feature with P/R suggestions
    
    * chore(client): Fix P/R comments
    
    ---------
    
    Co-authored-by: Jaume <singlavalls@media-saturn.com>

--- client/logic/ui/round_label.gd
+++ client/logic/ui/round_label.gd
@@ -4,7 +4,8 @@ extends Label
 @export var player_name: String:
 	set(value):
 		player_name = value
-		text = "%s %s %s" % [tr("PLAYER"), str(value), tr("TURN")]
+		# text = "%s %s %s" % [tr("PLAYER"), str(value), tr("TURN")]
+		text = tr("ANNOUNCEMENT_PLAYER_TURN").format({player_name= value})
 
 
 func _ready() -> void:
