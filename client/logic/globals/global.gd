ac27b7ee7561f3ee92104309a07cfae953c4b5c5
Stephan Kessler <stephan.kessler@outlook.com>
2024-10-02 15:00:48 +0200

    chore(client): added bootcamp map selection

--- client/logic/globals/global.gd
+++ client/logic/globals/global.gd
@@ -6,6 +6,8 @@ const CONFIG_SECTION: String = "game"
 const RENDERING_SECTION: String = "rendering"
 const MAP_FOLDER_PATH: String = "res://assets/maps/maps/"
 const MAP_CUSTOM_FOLDER_PATH: String = "user://maps/"
+const BOOTCAMP_FOLDER_PATH: String = "res://assets/maps/bootcamp/"
+const SCENARIO_FOLDER_PATH: String = "res://assets/maps/scenarios/"
 const SCENARIO_CUSTOM_FOLDER_PATH: String = "user://scenarios/"
 
 var selected_map_json: String
@@ -22,6 +24,8 @@ var gameover_scene: PackedScene = load("res://levels/game_over_screen.tscn")
 
 var config: ConfigFile = ConfigFile.new()
 var maps: Dictionary = {}
+var bootcamps: Dictionary = {}
+var scenarios: Dictionary = {}
 
 
 func save_config(key: String, value: Variant, section: String = CONFIG_SECTION) -> void:
@@ -33,6 +37,9 @@ func reload_maps() -> void:
 	maps.clear()
 	_load_maps(MAP_FOLDER_PATH)
 	_load_maps(MAP_CUSTOM_FOLDER_PATH)
+	_load_scenarios(BOOTCAMP_FOLDER_PATH, bootcamps)
+	_load_scenarios(SCENARIO_FOLDER_PATH, scenarios)
+	_load_scenarios(SCENARIO_CUSTOM_FOLDER_PATH, scenarios)
 
 
 func _ready() -> void:
@@ -57,4 +64,19 @@ func _load_maps(dir_path: String) -> void:
 		)
 		var map: Map = MapFile.deserialize(map_json)
 		maps[map.map_name] = map_json
+		map.queue_free()
+
+
+func _load_scenarios(dir_path: String, container: Dictionary) -> void:
+	var folder: DirAccess = DirAccess.open(dir_path)
+	for file_name: String in folder.get_files():
+		if not file_name.ends_with(".gls"):
+			continue
+		var data: Array[String] = ScenarioFile.load_from_file(
+			"%s/%s" % [folder.get_current_dir(), file_name]
+		)
+		var map_json: String = data[0]
+		var scenario_json: String = data[1]
+		var map: Map = MapFile.deserialize(map_json)
+		container[map.map_name] = {"map": map_json, "scenario": scenario_json}
 		map.queue_free()
